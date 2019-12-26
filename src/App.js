dcaba9de5f8c7072d3070e60ba00832e26607f58
Smritibajaj <simmybajaj1603@gmail.com>
2019-12-26 12:37:59 +0530

    initialize repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Reactivewars from './components/Reactivewars';
+
+function App() {
+  return (
+   <Reactivewars/>
+  );
+}
+
+export default App;
